ad3f289d13a63118cc3b122a69a4d20fda5a9951
casey fritsch <caseyfritsch@gmail.com>
2017-09-29 11:57:40 -0700

    add index

---
+++ csp_w_php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css"/>
+  </head>
+  <body>
+    <?php require_once('nav.php');
+ ?>
+ <div>
+    <video width="100%" autoplay>
+      <source src="assets/CorporateStoryProductionsConferenceVideoNeeds.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <h1>lets do this</h1>
+    <h1  class="title">Corporate Story Productions</h1>
+  </div>
+  </body>
+</html>
