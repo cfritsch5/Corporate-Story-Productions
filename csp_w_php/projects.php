22f96d6e89128ca6dcc2365e2a8ea5cf6d5ba4a4
casey fritsch <caseyfritsch@gmail.com>
2017-09-29 11:57:55 -0700

    add projects

---
+++ csp_w_php/projects.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css"/>
+  </head>
+  <body>
+    <?php require_once('nav.php');
+    ?>
+    <video width="100%" autoplay>
+      <source src="assets/CorporateStoryProductionsConferenceVideoNeeds.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <h1  class="title">Corporate Story Productions</h1>
+  </body>
+</html>
