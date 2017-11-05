5c1ce1d2e614827df863c83d4c31981c57dfd12b
casey fritsch <caseyfritsch@gmail.com>
2017-11-05 11:09:54 -0800

    add about header

---
+++ headers/aboutheader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutHeader = ()=>(
+  <div className="header">
+    <img src="assets/RobininOakland_hd_header.jpg"/>
+      <h1 className="tagline">About</h1>
+  </div>
+);
+
+export default AboutHeader;
