62dacbfd210eed812ee4f21f2f660401a4f67c08
casey fritsch <caseyfritsch@gmail.com>
2017-11-05 11:10:45 -0800

    add projects

---
+++ headers/projectsheader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProjectHeader = ()=>(
+  <div className="header">
+    <img src="assets/amphitheater_header.jpg"/>
+      <h1 className="tagline">Projects</h1>
+  </div>
+);
+
+export default ProjectHeader;
